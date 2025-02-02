f69909a372848199d0b32cae981a883e3256a979
bruno teixeira <brunofvteixeira@gmail.com>
2025-02-02 16:03:20 -0300

    1, 2, 3

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,9 @@
 # How many new outputs were created by block 123,456?
+blockhash=$(bitcoin-cli getblockhash 123456)
+block=$(bitcoin-cli getblock $blockhash 2)
+# echo $block | jq
+
+# array com todos vout de todos tx
+echo $block | jq '[.tx[].vout[]] | length'
+
+# done
